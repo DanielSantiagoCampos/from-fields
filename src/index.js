60b2dab233806ca148d7245d6953ff7d3b9dab51
DanielSantiagoCampos <camposdaniel1002@gmail.com>
2022-06-07 10:45:59 -0500

    add routes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import App from './App';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
